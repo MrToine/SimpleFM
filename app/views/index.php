225441775711fb200be96f2fa3131dc9584d18bf
Anhony <anthony.violet@outlook.fr>
2021-05-23 13:32:27 +0200

    Update index.php

--- app/views/index.php
+++ app/views/index.php
@@ -2,5 +2,5 @@
 <section class="spacing-5">
 	<p>Le framework c'est lancé correctement. Vous pouvez désormais coder votre site web en toute tranquillité ;)</p>
 	<a class="btn--primary" href="https://github.com/MrToine/SimpleFM">Lien Github du projet</a>
-	<a class="btn--success" href="https://github.com/MrToine/SimpleFM">Documentation</a>
+	<a class="btn--success" href="https://github.com/MrToine/SimpleFM/wiki/Pr%C3%A9sentation-g%C3%A9n%C3%A9rale-du-Framework" target="_blank">Documentation</a>
 </section>
